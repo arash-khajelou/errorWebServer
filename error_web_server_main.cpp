ff63ae6c8a53ae3d4dc9444414feb30dc641a576
Arash Khajelou <a.khajelou@gmail.com>
2013-03-09 15:39:09 +0330

    stableMain,unstable libs

--- error_web_server_main.cpp
+++ error_web_server_main.cpp
@@ -1,44 +1,37 @@
 #include "ServerSocket.h"
 #include "SocketException.h"
+#include "StringProc.h"
+
 #include <string>
 #include <iostream>
-int main ()
-{
-  std::cout << "Error Web Server ... !\n";
-  int i = 0 ;
-  std::string Data = "" ;
-  try
-    {
-      // Create the socket
-      ServerSocket server ( 3000 );
 
-      while ( true )
-	{
+int main () {
+ 	try { // starting the body of expection error
 
-	  ServerSocket new_sock;
-	  bool servTest = server.accept ( new_sock );
+    	ServerSocket server ( 2013 ); // creating socket in port 3000
 
-	  try
-	    {
-	      if ( servTest )
-		{
-		  Data = "<a href = \"http://www.google.com\" > salam </a> ";
-		  new_sock << Data ;
-		  new_sock << "\n<a href = \"http://www.google.com\"><h1>salam</h1></a>";
+      	while ( true ) { // main loop
+
+	  		ServerSocket connected_sock; // accepted socket created to client
+	  		bool servTest = server.accept ( connected_sock ); // generatig IP and port ...
+	  		std::string recvString = " ";
+	  		StringProc pStr ;
+	  		try {
+	      		if ( servTest ) {
+	      			if ( connected_sock.iRecv(recvString) ) {
+		  				pStr.setStringProc (recvString);
+		  				pStr.process();
+					}
+		  			//recvString = "<a href = \"http://www.google.com\" > google WebSie </a> ";
+		  			connected_sock << recvString ;
+		  			std::cout<< "\n" << pStr.getDir() << "\n" << pStr.getProto();
+				}
+	    	}
+			catch ( SocketException& ) {}
 		}
-	      if ( new_sock.iRecv(Data) )
-		{
-		  new_sock << Data ;
-		}
-	    }
-	  catch ( SocketException& ) {}
-
-	}
     }
-  catch ( SocketException& e )
-    {
-      std::cout << "Exception was caught:" << e.description() << "\nExiting.\n";
+    catch ( SocketException& e ) {
+    	std::cout << "Exception was caught:" << e.description() << "\nExiting.\n";
     }
-
-  return 0;
+	return 0;
 }
